30d1905e66fe85aa45f2b81f7406c285d3021113
Igor Yastrebov <i.hawk@yandex.ru>
2022-11-15 13:37:04 +0400

    Started work on different tokenizers

--- mtg.py
+++ mtg.py
@@ -11,6 +11,15 @@ whenever - watches for other effects
 '''
  
 import json
+
+IGNORE_TEXT = [
+    ' (Draw a card, then discard a card. If you discarded a nonland card, put a +1/+1 counter on this creature.)',
+    ' (Equipment, Auras you control, and counters are modifications.)',
+    ' (It\'s an artifact with "{T}, Sacrifice this artifact: Add one mana of any color.")',
+    ' (Create a Clue token. It\'s an artifact with "{2}, Sacrifice this artifact: Draw a card.)',
+    ' (They\'re artifacts with "{T}, Sacrifice this artifact: Add one mana of any color.")',
+    ' (The next time this creature would be destroyed, instead tap it, remove it from combat, and heal all damage on it.)'
+]
  
 CARDNAME_T = '[CARDNAME]'
  
@@ -49,21 +58,32 @@ class Card(JSONObject):
         result = Card()
         result.name = d['name']
         result.text = d['oracle_text']
-        result.text = result.text.replace(result.name, CARDNAME_T)
+        result.format_text()
         return result
 
     def print(self):
         print(self.name)
         print(f'\t{self.text}\n')
- 
-# effects that say 'if' or 'as long as'
-class Condition(JSONObject):
-    pass
- 
-class ManaPip(Condition):
-    def __init__(self) -> None:
-        pass
 
-class CostPart(JSONObject):
-    pass
+    def format_text(self):
+        # replace card name occurences
+        self.text = self.text.replace(self.name, CARDNAME_T)
+        # replace reminder text
+        for i in IGNORE_TEXT:
+            self.text = self.text.replace(i, '')
+
+# effects that say 'if' or 'as long as'
+class Cost(JSONObject):
+    def parse(s: str):
+        print(s)
+
+class Tap(JSONObject):
+    def __init__(self):
+        pass
  
+class ManaPip(Cost):
+    def __init__(self, s: str) -> None:
+        self.generic = 0
+        self.color: str = ''
+        self.lifeAltCost = 0
+        self.alt: ManaPip = None
